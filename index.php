9e89a717ddeae79dbf160090e4dd121541929448
Maxim Akimov <akimovmax@mail.ru>
2018-01-16 23:31:09 +0300

    Обращение к данным из json по ключам

--- index.php
+++ index.php
@@ -10,42 +10,41 @@
     $jsonHeadersData = file_get_contents($jsonPhonebookHeaders);
 
     // Преобразовываем данные из переменных в формат массивов:
-    $phoneBookArray = json_decode($jsonPhonebookData, true);
-    $headersArray = json_decode($jsonHeadersData, true);
+    $phoneBookArray = json_decode($jsonPhonebookData, false);
+    $headersArray = json_decode($jsonHeadersData, false);
 
     // Объединяем массив заголовков и массив данных:
     $dataArray = array_merge($headersArray, $phoneBookArray);
 ?>
 
 <!DOCTYPE html>
-    <html>
-        <title>Телефонная книга</title>
-        <meta charset='utf-8'>
-        <style>
-            table {
-                border-collapse: collapse;
-            }
-            tr:first-child {
-                text-align: center;
-                font-weight: 700;
-                background-color: darkgray;
-            }
-            td {
-                padding: 5px 10px;
-                border: 2px solid black;
-            }
-        </style>
-        <body>
-            <table>
-                <? foreach ($dataArray as $key => $array) { ?>
-                    <tr>
-                        <? foreach ($array as $wordKey => $value) { ?>
-                            <td>
-                                <?= $value ?>
-                            </td>
-                        <? } ?>
-                    </tr>
-                <? } ?>
-            </table>
-        </body>
-    </html>
+<html>
+    <title>Телефонная книга</title>
+    <meta charset='utf-8'>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        tr:first-child {
+            text-align: center;
+            font-weight: 700;
+            background-color: darkgray;
+        }
+        td {
+            padding: 5px 10px;
+            border: 2px solid black;
+        }
+    </style>
+    <body>
+        <table>
+            <? foreach ($dataArray as $key => $array) { ?>
+                <tr>
+                    <td><?= $dataArray[$key] -> {'firstName'} ?></td>
+                    <td><?= $dataArray[$key] -> {'lastName'} ?></td>
+                    <td><?= $dataArray[$key] -> {'address'} ?></td>
+                    <td><?= $dataArray[$key] -> {'phoneNumber'} ?></td>
+                </tr>
+            <? } ?>
+        </table>
+    </body>
+</html>
